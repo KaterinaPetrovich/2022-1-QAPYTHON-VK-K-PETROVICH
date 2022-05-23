1ec868e6ceea16c65dab2562a5e631cf8ffab58d
KaterinaPetrovich <katerina_petrovich@bk.ru>
2022-05-23 15:08:26 +0300

    deleted wrong files

--- homework1/helper.py
+++
@@ -1,9 +0,0 @@
-import locators
-
-
-def login(test):
-    test.driver.get("https://target.my.com/")
-    test.click(locators.ENTER_BUTTON_LOCATOR)
-    test.send_keys(locators.EMAIL_FIELD_LOCATOR, "katerina_petrovich@bk.ru")
-    test.send_keys(locators.PASSWORD_FIELD_LOCATOR, "135qwe")
-    test.click(locators.ENTER_LOGIN_BUTTON_LOCATOR)
